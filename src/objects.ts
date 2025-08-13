17a2a43aeac419303b1bb8207bbf39c717d8a0f0
Zhangir <zhangir@MacBook-Pro-Admin.local>
2025-08-13 22:32:20 +0500

    feat: working with objects

--- src/objects.ts
+++ src/objects.ts
@@ -1,12 +1,12 @@
 const tester = {
-  name: "Жангир",
-  city: "Almaty",
+  name: 'Жангир',
+  city: 'Almaty',
   age: 21,
-}
+};
 
-console.log(tester.name)
-tester.age = 24
+console.log(tester.name);
+tester.age = 24;
 
-for (let key in tester) {
+for (const key in tester) {
   console.log(key, tester[key]);
 }
